ad7fe91416d0dd3cbd1eae007f052e22f8b1460e
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2023-10-01 18:19:29 -0400

    need to use dynamic programming

--- longest_palindrome_substring.py
+++ longest_palindrome_substring.py
@@ -145,6 +145,42 @@ class Solution:
         
         return new_palindrome
 
+    def longest_palindrome_dynamic_programming(self, s: str) -> str:
+        n = len(s)
+        if n <= 1:
+            return s
+
+        # Create a table to store if substrings are palindromes
+        is_palindrome = [[False] * n for _ in range(n)]
+
+        start = 0  # Start index of the longest palindrome found so far
+        max_length = 1  # Length of the longest palindrome found so far
+
+        # All substrings of length 1 are palindromes
+        for i in range(n):
+            is_palindrome[i][i] = True
+
+        # Check for palindromes of length 2
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                is_palindrome[i][i + 1] = True
+                start = i
+                max_length = 2
+
+        # Check for palindromes of length 3 or more
+        for length in range(3, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if s[i] == s[j] and is_palindrome[i + 1][j - 1]:
+                    is_palindrome[i][j] = True
+                    if length > max_length:
+                        start = i
+                        max_length = length
+
+        return s[start:start + max_length]
+
+    def is_palindrome(self, s: str) -> bool:
+        return s == s[::-1]
 
 new_solution = Solution()
 s01 = "abcdzdcab"
